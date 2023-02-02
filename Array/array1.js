1059e05097aa9198e45675cb0fa50cfb8770c291
RAKIB <rakibmahmudmridha@gmail.com>
2023-02-02 16:14:40 +0600

    JS start

---
+++ Array/array1.js
@@ -0,0 +1,4 @@
+var arr = [1, 2, 3, 4, 5];
+
+console.log(arr[0]);
+console.log(arr[3]);
---
+++ hello.js
@@ -0,0 +1,88 @@
+// var n = 145;
+// var m = 200.50;
+
+// console.log(n + m);
+
+// var n = Number('54.44');
+// console.log( n );
+
+// console.log(Number.parseInt(n));
+
+// console.log(Number.Max_Value);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+// var abc;
+
+// var xyz = null;
+
+// console.log(abc);
+// console.log(xyz);
+
+// var str = '100';
+
+// var n = 10;
+
+// console.log( str * n );
+// console.log( str + n );
+
+// console.log ( Number.parseInt( str ) );
+// console.log( n.toString( n ));
+
+// var c = '50';
+// var d = 50;
+
+// console.log( c === d);
+// console.log( c !== d );
+// console.log( c == d );
+
+// var name = '';
+
+// var fullName = name || 'Rakib Mridha';
+// console.log(fullName);
+
+// var isOk = true;
+
+// isOk && console.log("Everything is okay");
+
+// isntOk = false;
+
+// isntOk && console.log("everything is not okay");
+
+// for( var i =  1; i <= 10; i++ )
+// {
+//     console.log(i+' ');
+// }
+
+// for( var i = 1; i <= 5; i++ )
+// {
+//    var result  = '';
+//     for( var j = 1; j <= i; j++ ){
+//         result += j + ' ';
+//     }
+//     console.log(result);
+// }
+
+//  var n = 10;
+
+//  var str = new String ( n );
+
+//  console.log(str);
+
+// console.log(str.toString());
+// s = str.toString(str);
+// console.log(s);
+
+// var n = 10;
+
+// var str = String(n);
+// console.log(str);
+
+// var str = 'This is a \'String\'.';
+// console.log(str);
+
+var a = "I am";
+var b = "Rakib Mridha";
+
+var c = a.concat(" ", b);
+
+console.log(c.charAt(3));
